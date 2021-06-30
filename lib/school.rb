08b37fa52de1c17de69c6b65e9b5b234322fdb30
Joyce Lee <joycelee901@gmail.com>
2021-06-29 21:20:09 -0500

    commit tree 11183151391f722d679c62c8d362eb2af3875afc

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if @roster.key?(grade)
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(num)
+        @roster[num].sort
+    end
+
+    def sort
+        @roster[grade].sort
+    end    
+end
